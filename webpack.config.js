d10189ef72539eaa8ec70ba6dd6c9b922337527e
Francis Chuang <francis.chuang@gmail.com>
2016-01-21 20:58:33 +1100

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack');
+var path = require('path')
+
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const TARGET = process.env.npm_lifecycle_event;
+
+module.exports = {
+
+    entry: {
+        app: ['./src/app.js'],
+    },
+
+    output: {
+        path: __dirname + '/build',
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js'
+    },
+
+    module: {
+        loaders: [
+            { test: /\.html$/, exclude: /node_modules/, loader: 'html'},
+            { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap&importLoaders=1!postcss-loader')},
+            { test: /\.png$/, exclude: /node_modules/, loader: 'file'}
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html"
+        }),
+
+        new ExtractTextPlugin('[id].[contenthash].css', { allChunks: true })
+    ],
+
+    postcss: function () {
+        return [
+            require('postcss-easysprites')()
+        ];
+    }
+};
